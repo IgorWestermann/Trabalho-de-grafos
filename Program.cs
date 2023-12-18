814cf48460af32d70610e294df389fb7960205e4
IgorWestermann <43771530+IgorWestermann@users.noreply.github.com>
2023-12-17 23:19:12 -0300

    Ordenacao topologica (#11)
    * algoritimo de ordenaçao topologica, mudança na estrutura do grafo
    
    * trabalho finalizado

--- Program.cs
+++ Program.cs
@@ -14,13 +14,13 @@ class Program
 
         do
         {
-            Console.WriteLine("Menu:");
+            Console.WriteLine("Menu");
             Console.WriteLine("1. Grafo Simples Não Valorado");
             Console.WriteLine("2. Grafo Simples Valorado");
             Console.WriteLine("3. Grafo Direcionado Valorado");
             Console.WriteLine("4. Exit");
 
-            Console.Write("Enter your choice (1-4): ");
+            Console.Write("Escolha um número (1-4)");
             if (int.TryParse(Console.ReadLine(), out choice))
             {
                 switch (choice)
@@ -38,15 +38,15 @@ class Program
                         Console.WriteLine("Saindo");
                         break;
                     default:
-                        Console.WriteLine("Número inválido. Escolha um número de 1 a 4");
+                        Console.WriteLine("Número inválido. Escolha um número(1-4)");
                         continue;
                 }
 
                 graph.ReadGraph($"./../../../{graphType}.txt", choice);
 
                 Console.WriteLine("\nOperações:");
-                Console.WriteLine("5. Transformar em Lista de Adjacencia");
-                Console.WriteLine("6. Transformar em Matriz de Distancia");
+                Console.WriteLine("5. Transformar em Matriz de Incidência");
+                Console.WriteLine("6. Transformar em Matriz de Adjacencia/Distancia");
                 Console.WriteLine("7. Transformar em Lista de Adjacencia");
                 Console.WriteLine("8. Busca em Largura (BFS)");
                 Console.WriteLine("9. Busca em Profundidade (DFS)");
@@ -54,34 +54,45 @@ class Program
                 Console.WriteLine("11. Prim");
                 Console.WriteLine("12. Ordenação Topologica");
                 Console.WriteLine("13. Ciclo Euleriano");
-                Console.WriteLine("0. Voltar");
+                Console.WriteLine("99. Voltar");
 
-                Console.Write("Encolha entre (5-11): ");
+                Console.Write("Encolha um número (5-13)");
                 if (int.TryParse(Console.ReadLine(), out choiceTwo))
                 {
                     var initialVertex = new int();
-                    var finalVertex = new int();
 
                     switch (choiceTwo)
                     {
                         case 5:
-                            GraphUtils.DisplayAdjacencyList(graph.TransformToAdjacencyList(choice));
+                            var matrizIncidencia = graph.TransformToIncidenceMatrix();
+                            GraphUtils.DisplayIncidenceTable(matrizIncidencia);
+                            GraphUtils.SalvarMatriz("Matriz_Incidencia_Output.txt", matrizIncidencia);
+                            GraphUtils.SalvarTabelaIncidencia("Tabela_Incidencia_Output.txt", matrizIncidencia);
                             break;
-                        
+
                         case 6:
-                            GraphUtils.DisplayAdjacencyMatrix(graph.TransformToDistanceMatrix());
+                            var matrizDistancia = graph.TransformToDistanceMatrix();
+                            GraphUtils.DisplayAdjacencyMatrix(matrizDistancia);
+                            if (choice == 3)
+                            {
+                                GraphUtils.SalvarMatriz("Matriz_Distancias_Output.txt", matrizDistancia);
+                                break;
+                            } 
+
+                            GraphUtils.SalvarMatriz("Matriz_Adjacencia_Output.txt", matrizDistancia);
                             break;
-                        
+
                         case 7:
-                            GraphUtils.DisplayIncidenceTable(graph.TransformToIncidenceMatrix());
+                            var listaDistancia = graph.TransformToAdjacencyList(choice);
+                            GraphUtils.DisplayAdjacencyList(listaDistancia);
+                            GraphUtils.SalvarLista("Lista_Adjacencia_Output.txt", listaDistancia);
                             break;
-                        
+
                         case 8:
                             Console.WriteLine("Escolha o vertice inicial");
 
                             initialVertex = int.Parse(Console.ReadLine());
-                            grafo.CriarGrafo(graph.AdjacencyMatrix);
-                            graph.ExecuteBFS(initialVertex, grafo);
+                            graph.ExecuteBFS(initialVertex, grafo.CriarGrafo(graph.AdjacencyMatrix));
 
                             break;
                         case 9:
@@ -96,23 +107,19 @@ class Program
                             Console.WriteLine("Escolha o vertice inicial");
                             initialVertex = int.Parse(Console.ReadLine());
 
-                            grafo.CriarGrafo(graph.AdjacencyMatrix);
                             graph.ExecuteDijkstra(graph.AdjacencyMatrix, initialVertex);
-
                             break;
                         case 11:
                             graph.ExecutePrim(grafo);
                             break;
                         case 12:
-                            graph.ExecuteTopologicalSort(grafo);
+                            graph.ExecuteTopologicalSort(grafo.CriarGrafo(graph.AdjacencyMatrix));
                             break;
                         case 13:
                             graph.ExecuteEulerianCycle(grafo);
                             break;
-                        case 14:
-                            break;
                         default:
-                            Console.WriteLine("Escolha um numero de 5 a 14");
+                            Console.WriteLine("Escolha um número (5-14)");
                             break;
                     }
                 }
@@ -125,6 +132,7 @@ class Program
             {
                 Console.WriteLine("Número inválido");
             }
+
             graph = new GraphUtils(0);
             grafo = new Grafo();
         } while (choice != 99);
